343e4a41c4d8184e0b0d4b67187f074d34649119
Craig Patik <craig@patik.com>
2023-05-20 18:16:54 +0200

    move the Next stuff to its own hook

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -2,6 +2,7 @@ import { NextRouter } from 'next/router'
 import { ReactElement, useCallback, useEffect, useMemo, useState } from 'react'
 import { DeferUntilInteractionContext } from './Context'
 import { ProviderProps } from './types'
+import { useTrackNextRouter } from './useTrackNextRouter'
 
 // These are the events that we assume will occur when the user interacts with the page
 const eventNames = ['click', 'touchstart', 'scroll']
@@ -11,9 +12,9 @@ const eventNames = ['click', 'touchstart', 'scroll']
  *
  * Returns a boolean as well as a function which takes a callback that will only be invoked when the user has interacted with the page
  *
- * Pass useRouter if you want it to reset the value when the route changes (recommended for Next.js apps)
+ * Pass a NextRouter if you want it to reset the value when the route changes (recommended for Next.js apps)
  */
-export function Provider({ useRouter, children }: ProviderProps & { useRouter?: () => NextRouter }): ReactElement {
+export function Provider({ router, children }: ProviderProps & { router?: NextRouter }): ReactElement {
     const [hasInteracted, setHasInteracted] = useState(false)
     const [areEventListenersCurrentlyActive, setAreEventListenersCurrentlyActive] = useState(false)
     const handleInteraction = () => {
@@ -42,37 +43,14 @@ export function Provider({ useRouter, children }: ProviderProps & { useRouter?: 
         return () => removeEventListeners()
     }, [removeEventListeners, addEventListeners])
 
-    const onRouteChangeStart = useCallback(() => {
-        console.log('xyz resetting value because of route change')
-
-        // If the user hasn't interacted since the last route change, then it means we haven't cleaned up the existing event listeners yet, so do that first before we re-add them below
-        if (areEventListenersCurrentlyActive) {
-            removeEventListeners()
-        }
-
-        setHasInteracted(false)
-    }, [areEventListenersCurrentlyActive, removeEventListeners])
-
-    const onRouteChangeComplete = useCallback(() => {
-        // This tiny delay allows us to skip past the scroll event that will be fired when Next scrolls the page to the top
-        requestAnimationFrame(() => addEventListeners())
-    }, [addEventListeners])
-
-    const router = useRouter ? useRouter() : null
-
-    useEffect(() => {
-        if (!router) {
-            return
-        }
-
-        router.events.on('routeChangeStart', onRouteChangeStart)
-        router.events.on('routeChangeComplete', onRouteChangeComplete)
-
-        return () => {
-            router.events.off('routeChangeStart', onRouteChangeStart)
-            router.events.off('routeChangeComplete', onRouteChangeComplete)
-        }
-    }, [onRouteChangeComplete, onRouteChangeStart, router?.events])
+    // Optionally watch a NextRouter for route changes
+    useTrackNextRouter(
+        areEventListenersCurrentlyActive,
+        removeEventListeners,
+        setHasInteracted,
+        addEventListeners,
+        router
+    )
 
     // This function will only invoke its callback when the page has been interacted with
     const afterInteraction = useMemo(
---
+++ src/useTrackNextRouter.tsx
@@ -0,0 +1,40 @@
+import { NextRouter } from 'next/router'
+import { Dispatch, SetStateAction, useCallback, useEffect } from 'react'
+
+export function useTrackNextRouter(
+    areEventListenersCurrentlyActive: boolean,
+    removeEventListeners: () => void,
+    setHasInteracted: Dispatch<SetStateAction<boolean>>,
+    addEventListeners: () => void,
+    router: NextRouter | undefined
+) {
+    const onRouteChangeStart = useCallback(() => {
+        console.log('xyz resetting value because of route change')
+
+        // If the user hasn't interacted since the last route change, then it means we haven't cleaned up the existing event listeners yet, so do that first before we re-add them below
+        if (areEventListenersCurrentlyActive) {
+            removeEventListeners()
+        }
+
+        setHasInteracted(false)
+    }, [areEventListenersCurrentlyActive, removeEventListeners])
+
+    const onRouteChangeComplete = useCallback(() => {
+        // This tiny delay allows us to skip past the scroll event that will be fired when Next scrolls the page to the top
+        requestAnimationFrame(() => addEventListeners())
+    }, [addEventListeners])
+
+    useEffect(() => {
+        if (!router) {
+            return
+        }
+
+        router.events.on('routeChangeStart', onRouteChangeStart)
+        router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+        return () => {
+            router.events.off('routeChangeStart', onRouteChangeStart)
+            router.events.off('routeChangeComplete', onRouteChangeComplete)
+        }
+    }, [onRouteChangeComplete, onRouteChangeStart, router?.events])
+}
